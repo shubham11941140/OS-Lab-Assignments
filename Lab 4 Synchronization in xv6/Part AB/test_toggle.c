31cc593dd610d577a025e2861fd160ea915e507b
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2021-06-30 12:04:53 +0530

    Add files via upload

---
+++ Lab 4 Synchronization in xv6/Part AB/test_toggle.c
@@ -0,0 +1,48 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main()
+{
+	int ret;
+	int i;
+	uspinlock_init();
+	ucounter_init();
+
+	ret = fork();
+	if(ret == 0)
+	{
+		for(i=0; i < 10; i++)
+		{
+			uspinlock_acquire(0);
+			while(ucounter_get(0) == 1)
+			{
+				ucv_sleep(0, 0);
+			}
+			printf(1, "I am child\n");
+			ucounter_set(0,1);
+			ucv_wakeup(1);
+			uspinlock_release(0);
+		}
+		exit();
+	}
+	else
+	{
+		for(i=0; i < 10; i++)
+		{
+			uspinlock_acquire(0);
+			while(ucounter_get(0) == 0)
+			{
+				ucv_sleep(1,0);
+			}
+			printf(1, "I am parent\n");
+			ucounter_set(0,0);
+			ucv_wakeup(0);
+			uspinlock_release(0);
+
+		}
+		wait();
+		exit();
+	}
+}
+
